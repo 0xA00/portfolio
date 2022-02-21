c7f5fa21f4cc5519f1de9166bacdc442bb4cc40a
Lengthrequired <hyp3rcr4ft@gmail.com>
2022-02-21 11:51:28 +0100

    terminal stat added

--- js/Frontpage.js
+++ js/Frontpage.js
@@ -1,9 +1,12 @@
 var i =0;
 function alternate(){
   var doc = document.getElementById("whitespace1");
+  var doc2 = document.getElementById("whitespace2");
   var color = ["#300a24","white"];
   doc.style.backgroundColor = color[i];
+  doc2.style.backgroundColor = color[i];
   doc.style.color=color[i];
+  doc2.style.color=color[i];
   i = (i+1)%color.length;
 }
 setInterval(alternate,1000);
